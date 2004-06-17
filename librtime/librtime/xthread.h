ea957bfb164b173aa1741b9d6f21582e2a94a498
hling <hling@d0eb7cca-e9da-0310-b5ff-beb5b2e817b2>
2004-06-17 00:51:42 +0000

    rename to xthread_t

--- librtime/librtime/xthread.h
+++ librtime/librtime/xthread.h
@@ -23,11 +23,14 @@
 
 #define XTHREAD_NONEFLAGS 0 
  
+/* Need to change name, remove xrtp prefix */
 typedef struct xrtp_thread_s xrtp_thread_t;
 typedef struct xrtp_lock_s xthr_lock_t;
 typedef struct xrtp_cond_s xthr_cond_t;
 
-extern DECLSPEC  xrtp_thread_t * xthr_new(int (*fn)(void *), void *, int flag);
+typedef struct xrtp_thread_s xthread_t;
+
+extern DECLSPEC  xthread_t * xthr_new(int (*fn)(void *), void *, int flag);
 
 extern DECLSPEC  int xthr_wait(xrtp_thread_t * anoth, void *ret_anoth);
 
