072466d1249f13916a94da92ca59454b9f1480be
hling <hling@d0eb7cca-e9da-0310-b5ff-beb5b2e817b2>
2005-01-12 07:34:56 +0000

    Possible to bypass int8, uint8, etc definition, in case defined by others such as npapi

--- libtmdbase/libtmdbase/os.h
+++ libtmdbase/libtmdbase/os.h
@@ -95,6 +95,10 @@
  typedef long              off_t;
 #endif
 
+#ifndef OS_IGNORE_INTEGER_TYPE
+/**
+ * This may be a solution duo to npapi has conflict typedef with these
+ */
 typedef unsigned int   uint32;
 typedef unsigned short uint16;
 typedef unsigned char  uint8;
@@ -102,6 +106,7 @@ typedef unsigned char  uint8;
 typedef int   int32;
 typedef short int16;
 typedef char  int8;
+#endif
 
 /* Some compilers use a special export keyword 
  * Copy from SDL (www.libsdl.org)
